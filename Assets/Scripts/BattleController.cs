71dcb18160976820464efb570e8ed5741995fbef
NastyTester <70013669+AlinNast@users.noreply.github.com>
2022-07-26 20:42:01 +0300

    Added first hero

---
+++ Assets/Scripts/BattleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleController : MonoBehaviour
+{
+    public int testThree = 5;
+    private int testFour;
+ 
+    void Start()
+    {
+        //testThree = 25;
+        //print("testThree = " + testThree);
+        int largets = 7;
+        print(largets);
+
+    }
+    private void ExampleOne()
+    {
+        int testOne = 10;
+        string testTwo = "Delete Later";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Method()
+    {
+       ///* public*/ int Ups = 5;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # SmartTactics
 This is a turn based strategy game developed with unity
+
+## It features:
+ - 16 x 19 hex map
+ - One hero with animatons
+
+## How to install:
+ - Download the repo and open it with Unity (soon an .exe will be available)
+
+## What I learned:
+ - What are prefabs
+ - How to link C# scripts to game objects
+ - How to do a basic cartoonish animation
