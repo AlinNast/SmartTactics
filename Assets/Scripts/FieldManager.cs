c8d1099fc1c8408b331fa445fea114e43999774a
NastyTester <70013669+AlinNast@users.noreply.github.com>
2022-08-07 23:16:32 +0300

    added coordinate system to map

--- Assets/Scripts/FieldManager.cs
+++ Assets/Scripts/FieldManager.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class FieldManager : MonoBehaviour
 {
+    public RowManager[] allRows;
+    public BattleHex[,] allHexesArray;
+    int allRowsLength;
   
     void Start()
     {
-
-
+        allRows = GetComponentsInChildren<RowManager>();
+        allRowsLength = allRows.Length;
+        for(int i = 0; i < allRowsLength; i++)
+        {
+            allRows[i].allHexesInRow = allRows[i].GetComponentsInChildren<BattleHex>();
+        }
+        CreateAllHexesArray();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateAllHexesArray()
     {
+        int heightOfArray = allRows.Length;
+        int widthOfArray = allRows[0].allHexesInRow.Length;
+        allHexesArray = new BattleHex[widthOfArray, heightOfArray];
+
+        for(int j = 0; j < heightOfArray; j++)
+        {
+            for (int i = 0; i < widthOfArray; i++)
+            {
+                allHexesArray[i, j] = allRows[heightOfArray-1- j].allHexesInRow[widthOfArray - 1 - i];
+                allHexesArray[i, j].verticalCoordinate = j+1;
+                allHexesArray[i, j].horizontalCoordinate = i + 1;
+            }
+        }
         
     }
 }
---
+++ Assets/Scripts/Hexes/BattleHex.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum HexState { inactive, active};
+
+public class BattleHex : MonoBehaviour
+{
+    public int horizontalCoordinate;
+    public int verticalCoordinate;
+    public HexState hexState;
+    public bool isSecondLevel;
+}
